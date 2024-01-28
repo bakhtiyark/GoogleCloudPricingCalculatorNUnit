fba308d5863256f7887260b667ec8c3c7fe8f75f
Bakhtiyar Kussainov <bakhtiyar_kussainov@epam.com>
2024-01-29 01:18:52 +0500

    Do unit tests

--- GoogleCloudPricingCalculatorNUnit/PageObjects/Components/Calculator/EstimateBlock.cs
+++ GoogleCloudPricingCalculatorNUnit/PageObjects/Components/Calculator/EstimateBlock.cs
@@ -1,6 +1,34 @@
 namespace GoogleCloudPricingCalculatorNUnit.PageObjects.Components.Calculator;
 
-public class EstimateBlock
+public class EstimateBlock(IWebDriver driver) : BaseComponent(driver, "#resultBlock")
 {
-    
+    private readonly ComputeEngineEstimateComponent ComputerEngineEstimate = new(driver);
+    private readonly SendEstimateEmailComponent SendEstimate = new(driver);
+
+    public IWebElement EmailFormButton => driver.FindElement(By.XPath("//button[@id='Email Estimate']"));
+
+    public void OpenEmailForm()
+    {
+        EmailFormButton.Click();
+        var handles = driver.WindowHandles.ToList();
+        driver.SwitchTo().Window(handles[0]);
+    }
+
+    public string GetData(string target, int index)
+    {
+        var itemElement = ComputerEngineEstimate.GetEstimateItem(target);
+        var itemElementTextContent = itemElement.Text;
+        var result = itemElementTextContent.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[index];
+        return result;
+    }
+
+    public void SendEstimateMessage()
+    {
+        Actions actions = new Actions(driver);
+        driver.SwitchTo().Frame(driver.FindElement(By.XPath("//devsite-iframe//iframe")));
+        driver.SwitchTo().Frame(driver.FindElement(By.CssSelector("#myFrame")));
+        SendEstimate.Email.Click();
+        actions.KeyDown(Keys.Control).SendKeys("v").KeyUp(Keys.Control).Build();
+        SendEstimate.SendEstimateButton.Click();
+    }
 }
--- GoogleCloudPricingCalculatorNUnit/PageObjects/Components/Calculator/EstimateBlock/SendEstimate.cs
+++ GoogleCloudPricingCalculatorNUnit/PageObjects/Components/Calculator/EstimateBlock/SendEstimate.cs
@@ -1,8 +1,8 @@
 namespace GoogleCloudPricingCalculatorNUnit.PageObjects.Components.Calculator;
 
-public class SendEstimate(IWebDriver driver) : BaseComponent(driver, "form[name=\"emailForm\"]")
+public class SendEstimateEmailComponent(IWebDriver driver): BaseComponent(driver, "form[name='emailForm'")
 {
-    // TODO: Perhaps i should find a better name
-    public IWebElement SendEstimateButton => RootElement.FindElement(By.XPath("//button[contains(text(),\"Send Email\")]"));
-    public IWebElement EmailInput => RootElement.FindElement(By.XPath("//input[@ng-model=\"emailQuote.user.email\"]"));
+    private readonly IWebDriver _driver = driver;
+    public IWebElement SendEstimateButton => _driver.FindElement(By.XPath("//button[contains(text(),\"Send Email\")]"));
+    public IWebElement Email => _driver.FindElement(By.XPath("//input[@ng-model='emailQuote.user.email']"));
 }
